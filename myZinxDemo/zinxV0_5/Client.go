26b4397e6d0324dd5845562c26c85ffa10c624a4
jinxing.f <jinxing.f@gmail.com>
2022-03-09 22:28:01 +0800

    add client test code

--- myZinxDemo/zinxV0_5/Client.go
+++ myZinxDemo/zinxV0_5/Client.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"time"
+	"zinx/znet"
 )
 
 func main() {
@@ -17,20 +19,45 @@ func main() {
 	}
 
 	for {
-		_, err := conn.Write([]byte("hello, Zinx v0.5 ..."))
+		// message struct
+		dp := znet.DataPack{}
+		binaryMsg, err := dp.Pack(znet.NewMsgPackage(0, []byte("Zinx0.5 client test message")))
 		if err != nil {
-			fmt.Println("write to server error, ", err)
+			fmt.Println("pack message error ", err)
+			return
+		}
+		if _, err = conn.Write(binaryMsg); err != nil {
+			fmt.Println("write message error ", err)
 			return
 		}
 
-		buf := make([]byte, 512)
-		cnt, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("read from server error, ", err)
-			return
+		// server will write back some message
+		// read header data, get msg id and data length
+		// read data by data length
+		binaryHead := make([]byte, dp.GetHeadLen())
+		if _, err := io.ReadFull(conn, binaryHead); err != nil {
+			fmt.Println("read data err ", err)
+			break
 		}
-		fmt.Printf("server call back: %s, cnt : %d\n", buf, cnt)
+		// unpack bianary header
+		msgHeader, err := dp.Unpack(binaryHead)
+		if err != nil {
+			fmt.Println("unpack header data err ", err)
+			break
+		}
+		if msgHeader.GetMsgLen() > 0 {
+			// read msg data second
+			msg := msgHeader.(*znet.Message)
+			msg.Data = make([]byte, msg.GetMsgLen())
 
+			if _, err := io.ReadFull(conn, msg.Data); err != nil {
+				fmt.Println("read message data err ", err)
+				return
+			}
+
+			fmt.Println("Recv server msg, Msg ID = ", msg.GetMsgId(), ", length is ", msg.GetMsgLen(),
+				" data is ", string(msg.GetData()))
+		}
 		// sleep for break
 		time.Sleep(1 * time.Second)
 	}
--- myZinxDemo/zinxV0_5/Server.go
+++ myZinxDemo/zinxV0_5/Server.go
@@ -15,10 +15,6 @@ type PingRouter struct {
 
 func (p *PingRouter) Handle(request ziface.IRequest) {
 	fmt.Println("Call router Handle...")
-	_, err := request.GetConnection().GetTCPConnection().Write([]byte("ping...ping...ping\n"))
-	if err != nil {
-		fmt.Println("Call back ping...ping...ping")
-	}
 	fmt.Println("recv from client, MsgId: ", request.GetMsgId(), ", msgData ", string(request.GetData()))
 	// request args has connection and data, and write back something
 	if err := request.GetConnection().SendMsg(1, []byte("ping...ping...ping")); err != nil {
