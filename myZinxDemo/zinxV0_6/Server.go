74267662f7b18131bc9f0bba4723575606a316f0
jinxing.f <jinxing.f@gmail.com>
2022-03-10 23:07:15 +0800

    add client test
    add client and server test demo

--- myZinxDemo/zinxV0_6/Server.go
+++ myZinxDemo/zinxV0_6/Server.go
@@ -23,10 +23,26 @@ func (p *PingRouter) Handle(request ziface.IRequest) {
 
 }
 
-func main() {
-	s := znet.NewServer("[Zinx V0.]")
+type HelloRouter struct {
+	// inherit
+	znet.BaseRouter
+}
 
-	s.AddRouter(&PingRouter{})
+func (h *HelloRouter) Handle(request ziface.IRequest) {
+	fmt.Println("Call hello router Handle...")
+	fmt.Println("recv from client, MsgId: ", request.GetMsgId(), ", msgData ", string(request.GetData()))
+	// request args has connection and data, and write back something
+	if err := request.GetConnection().SendMsg(201, []byte("hello...hello...hello")); err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func main() {
+	s := znet.NewServer("[Zinx V0.6]")
+
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloRouter{})
 
 	s.Server()
 }
